d72710fbf60b2f9a7887a3b61c99fb7e2ba11bac
zhouzhou <is-zhou@outlook.com>
2025-09-14 15:19:41 +0800

    feat: 插槽渲染

--- src/lib/FormRender.vue
+++ src/lib/FormRender.vue
@@ -58,17 +58,50 @@ const renderNode = (node: TComponentConfig) => {
       default: () => node.children?.map(renderNode),
     });
   }
+  let slots: { [key: string]: () => Array<VNode> } | undefined = undefined;
+
+  if (Array.isArray(node.slots) && node.slots.length > 0) {
+    // {
+    //   slotName: () => [
+    //     h("option", { value: "shanghai" }, "Zone one"),
+    //     h("option", { value: "beijing" }, "Zone two"),
+    //   ];
+    // }
+    slots = {};
+
+    node.slots.forEach((slot) => {
+      const slotComp = getComponent(slot.componentName);
+      if (!slotComp) {
+        return;
+      }
+
+      if (slot.options) {
+        const list = slot.options?.map((opt) => {
+          const { value, name, label } = opt;
+          return h(slotComp, { value, name }, { default: () => label });
+        });
+
+        slots![slot.name] = () => list;
+      } else {
+        slots![slot.name] = () => [h(slot.componentName)];
+      }
+    });
+  }
 
   return h(
     ElFormItem,
     { prop: node.field, ...node.formItemAttrs },
     {
       default: () =>
-        h(comp, {
-          ...node.attrs,
-          modelValue: formData.value[node.field],
-          "onUpdate:modelValue": (v) => (formData.value[node.field] = v),
-        }),
+        h(
+          comp,
+          {
+            ...node.attrs,
+            modelValue: formData.value[node.field],
+            "onUpdate:modelValue": (v) => (formData.value[node.field] = v),
+          },
+          slots
+        ),
     }
   );
 };
