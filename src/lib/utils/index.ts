97c620d2651f3c34b93e043047f45178df1aaa0a
zhouzhou <is-zhou@outlook.com>
2025-10-04 15:12:16 +0800

    chore: 设置placeholder

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -10,6 +10,13 @@ export function setDefaultValues(formData: Record<string, unknown>, componentCon
             if (typeof config.defaultValue !== "undefined" && get(_formData, field) === undefined && isVisible) {
                 set(_formData, field, config.defaultValue);
             }
+            if (!config.attrs.placeholder) {
+                const text: { [key: string]: string } = {
+                    select: "选择",
+                }
+                config.attrs.placeholder = `请${text[config.componentName] || '输入'}${config.formItemAttrs.label}`
+            }
+
         } else if (config.children?.length) {
             setDefaultValues(_formData, config.children);
         }
