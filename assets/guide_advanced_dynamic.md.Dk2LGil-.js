7692ad6bf493b4c12c308dafc8820bb14e383a14
is-zhou <is-zhou@users.noreply.github.com>
2025-10-10 03:13:26 +0000

    deploy: 908947c6cf10548e4bb07f088a600bfd7ee8377f

---
+++ assets/guide_advanced_dynamic.md.Dk2LGil-.js
@@ -0,0 +1,209 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/chunks/Config.BHPMbp9E.js","assets/chunks/framework.Cd6hzTNw.js","assets/chunks/Readonly.BiDuzX5v.js","assets/chunks/Disabled.s5BzerSB.js","assets/chunks/Visible.GjkPmAhH.js"])))=>i.map(i=>d[i]);
+import{D as d,v as m,aX as p,p as _,C as W,c as Z,o as C,j as a,aW as g,a as s,ag as u,G as n,ah as h,k as t,w as r,ax as c}from"./chunks/framework.Cd6hzTNw.js";import{L as f,N as k}from"./chunks/index.BpdcUPsw.js";const D=`<script setup lang="ts">
+import { ref } from "vue";
+import type { FormSchema } from "form-cook-render";
+
+const formData = ref({});
+const formSchema = ref<FormSchema>({
+  formAreaConfig: {
+    defaultCreateBtn: "提交",
+    defaultRestBtn: "重置",
+    attrs: {
+      labelWidth: "80px",
+    },
+  },
+  formContentConfigList: [
+    {
+      id: "1",
+      componentName: "Switch",
+      componentType: "form",
+      formItemAttrs: { field: "delivery", label: "即时配送" },
+      attrs: {},
+      defaultValue: false,
+    },
+    {
+      id: "2",
+      componentName: "Input",
+      componentType: "form",
+      formItemAttrs: { field: "name", label: "姓名" },
+      attrs: {
+        placeholder: "请输入姓名",
+      },
+      defaultValue: "齐天大圣",
+      visible: (prams) => {
+        if (prams.formData.delivery === true) {
+          prams.schemaItem.formItemAttrs.label = "配送员";
+        } else {
+          prams.schemaItem.formItemAttrs.label = "姓名";
+        }
+        return true;
+      },
+    },
+  ],
+});
+<\/script>
+
+<template>
+  <form-cook-render
+    v-model="formData"
+    v-model:form-schema="formSchema"
+    @submit="(data) => console.log('提交数据:', data)"
+    @reset="console.log('表单已重置')"
+  />
+</template>
+`,A=`<script setup lang="ts">
+import { ref } from "vue";
+import type { FormSchema } from "form-cook-render";
+
+const formData = ref({});
+const formSchema = ref<FormSchema>({
+  formAreaConfig: {
+    defaultCreateBtn: "提交",
+    defaultRestBtn: "重置",
+    attrs: {
+      labelWidth: "80px",
+    },
+  },
+  formContentConfigList: [
+    {
+      id: "1",
+      componentName: "Switch",
+      componentType: "form",
+      formItemAttrs: { field: "delivery", label: "即时配送" },
+      attrs: {},
+      defaultValue: false,
+    },
+    {
+      id: "2",
+      componentName: "Input",
+      componentType: "form",
+      formItemAttrs: { field: "name", label: "姓名" },
+      attrs: {
+        placeholder: "请输入姓名",
+        readonly: (prams) => {
+          return !prams.formData.delivery === true;
+        },
+      },
+      defaultValue: "齐天大圣",
+    },
+  ],
+});
+<\/script>
+
+<template>
+  <form-cook-render
+    v-model="formData"
+    v-model:form-schema="formSchema"
+    @submit="(data) => console.log('提交数据:', data)"
+    @reset="console.log('表单已重置')"
+  />
+</template>
+`,T=`<script setup lang="ts">
+import { ref } from "vue";
+import type { FormSchema } from "form-cook-render";
+
+const formData = ref({});
+const formSchema = ref<FormSchema>({
+  formAreaConfig: {
+    defaultCreateBtn: "提交",
+    defaultRestBtn: "重置",
+    attrs: {
+      labelWidth: "80px",
+    },
+  },
+  formContentConfigList: [
+    {
+      id: "1",
+      componentName: "Switch",
+      componentType: "form",
+      formItemAttrs: { field: "delivery", label: "即时配送" },
+      attrs: {},
+      defaultValue: false,
+    },
+    {
+      id: "2",
+      componentName: "Input",
+      componentType: "form",
+      formItemAttrs: { field: "name", label: "姓名" },
+      attrs: {
+        placeholder: "请输入姓名",
+        disabled: (prams) => {
+          return !prams.formData.delivery === true;
+        },
+      },
+    },
+  ],
+});
+<\/script>
+
+<template>
+  <form-cook-render
+    v-model="formData"
+    v-model:form-schema="formSchema"
+    @submit="(data) => console.log('提交数据:', data)"
+    @reset="console.log('表单已重置')"
+  />
+</template>
+`,V=`<script setup lang="ts">
+import { ref } from "vue";
+import type { FormSchema } from "form-cook-render";
+
+const formData = ref({});
+const formSchema = ref<FormSchema>({
+  formAreaConfig: {
+    defaultCreateBtn: "提交",
+    defaultRestBtn: "重置",
+    attrs: {
+      labelWidth: "80px",
+    },
+  },
+  formContentConfigList: [
+    {
+      id: "1",
+      componentName: "Switch",
+      componentType: "form",
+      formItemAttrs: { field: "delivery", label: "即时配送" },
+      attrs: {},
+      defaultValue: true,
+    },
+    {
+      id: "2",
+      componentName: "Input",
+      componentType: "form",
+      formItemAttrs: { field: "name", label: "姓名" },
+      attrs: {
+        placeholder: "请输入姓名",
+      },
+      defaultValue: "齐天大圣",
+      visible: (prams) => {
+        return prams.formData.delivery === true;
+      },
+    },
+  ],
+});
+<\/script>
+
+<template>
+  <form-cook-render
+    v-model="formData"
+    v-model:form-schema="formSchema"
+    @submit="(data) => console.log('提交数据:', data)"
+    @reset="console.log('表单已重置')"
+  />
+</template>
+`,F=JSON.parse('{"title":"组件联动","description":"","frontmatter":{},"headers":[],"relativePath":"guide/advanced/dynamic.md","filePath":"guide/advanced/dynamic.md"}'),B={name:"guide/advanced/dynamic.md"},L=Object.assign(B,{setup(I){const y=d();m(async()=>{y.value=(await p(async()=>{const{default:i}=await import("./chunks/Config.BHPMbp9E.js");return{default:i}},__vite__mapDeps([0,1]))).default});const v=d();m(async()=>{v.value=(await p(async()=>{const{default:i}=await import("./chunks/Readonly.BiDuzX5v.js");return{default:i}},__vite__mapDeps([2,1]))).default});const E=d();m(async()=>{E.value=(await p(async()=>{const{default:i}=await import("./chunks/Disabled.s5BzerSB.js");return{default:i}},__vite__mapDeps([3,1]))).default});const l=_(!0),b=d();return m(async()=>{b.value=(await p(async()=>{const{default:i}=await import("./chunks/Visible.GjkPmAhH.js");return{default:i}},__vite__mapDeps([4,1]))).default}),(i,e)=>{const o=W("ClientOnly");return C(),Z("div",null,[e[11]||(e[11]=a("h1",{id:"组件联动",tabindex:"-1"},[s("组件联动 "),a("a",{class:"header-anchor",href:"#组件联动","aria-label":'Permalink to "组件联动"'},"​")],-1)),e[12]||(e[12]=a("h2",{id:"显示隐藏联动-visible",tabindex:"-1"},[s("显示隐藏联动 visible "),a("a",{class:"header-anchor",href:"#显示隐藏联动-visible","aria-label":'Permalink to "显示隐藏联动 visible"'},"​")],-1)),e[13]||(e[13]=a("p",null,[s("表单组件和布局组件都可通过 "),a("code",null,"visible"),s(" 和数据进行动态控制"),a("code",null,"显示隐藏")],-1)),a("ul",null,[a("li",null,[e[4]||(e[4]=s("开启即时配送才需要输入姓名",-1)),u(n(t(f),null,null,512),[[h,l.value]]),n(o,null,{default:r(()=>[n(t(k),{title:"",description:"",locale:"",select:"vue",order:"vue,react,html",github:"",gitlab:"",theme:"",lightTheme:"",darkTheme:"",stackblitz:"%7B%22show%22%3Afalse%7D",codesandbox:"%7B%22show%22%3Afalse%7D",codeplayer:"%7B%22show%22%3Afalse%7D",files:"%7B%22vue%22%3A%7B%7D%2C%22react%22%3A%7B%7D%2C%22html%22%3A%7B%7D%7D",scope:"",htmlWriteWay:"write",background:"undefined",visible:!0,onMount:e[0]||(e[0]=()=>{l.value=!1}),vueCode:t(V)},c({_:2},[b.value?{name:"vue",fn:r(()=>[n(t(b))]),key:"0"}:void 0]),1032,["vueCode"])]),_:1})])]),e[14]||(e[14]=g(`<div class="language-ts vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">ts</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">visible</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">(params{</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  formData: Record</span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">&lt;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">string, any&gt;;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  schemaItem: ComponentConfig;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}) {</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">    return</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> params.formData.count </span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">===</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> &quot;1&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}</span></span></code></pre></div><h2 id="表单禁用联动-disabled" tabindex="-1">表单禁用联动 disabled <a class="header-anchor" href="#表单禁用联动-disabled" aria-label="Permalink to &quot;表单禁用联动 disabled&quot;">​</a></h2>`,2)),a("ul",null,[a("li",null,[e[5]||(e[5]=s("开启即时配送才可以输入姓名",-1)),u(n(t(f),null,null,512),[[h,l.value]]),n(o,null,{default:r(()=>[n(t(k),{title:"",description:"",locale:"",select:"vue",order:"vue,react,html",github:"",gitlab:"",theme:"",lightTheme:"",darkTheme:"",stackblitz:"%7B%22show%22%3Afalse%7D",codesandbox:"%7B%22show%22%3Afalse%7D",codeplayer:"%7B%22show%22%3Afalse%7D",files:"%7B%22vue%22%3A%7B%7D%2C%22react%22%3A%7B%7D%2C%22html%22%3A%7B%7D%7D",scope:"",htmlWriteWay:"write",background:"undefined",visible:!0,onMount:e[1]||(e[1]=()=>{l.value=!1}),vueCode:t(T)},c({_:2},[E.value?{name:"vue",fn:r(()=>[n(t(E))]),key:"0"}:void 0]),1032,["vueCode"])]),_:1})])]),e[15]||(e[15]=g(`<div class="language-ts vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">ts</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">disabled</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">(params{</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  formData: Record</span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">&lt;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">string, any&gt;;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  schemaItem: ComponentConfig;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}) {</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">    return</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> params.formData.count </span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">===</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> &quot;1&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}</span></span></code></pre></div><h2 id="表单只读联动-readonly" tabindex="-1">表单只读联动 readonly <a class="header-anchor" href="#表单只读联动-readonly" aria-label="Permalink to &quot;表单只读联动 readonly&quot;">​</a></h2>`,2)),a("ul",null,[a("li",null,[e[6]||(e[6]=s("开启即时配送才可以修改姓名",-1)),u(n(t(f),null,null,512),[[h,l.value]]),n(o,null,{default:r(()=>[n(t(k),{title:"",description:"",locale:"",select:"vue",order:"vue,react,html",github:"",gitlab:"",theme:"",lightTheme:"",darkTheme:"",stackblitz:"%7B%22show%22%3Afalse%7D",codesandbox:"%7B%22show%22%3Afalse%7D",codeplayer:"%7B%22show%22%3Afalse%7D",files:"%7B%22vue%22%3A%7B%7D%2C%22react%22%3A%7B%7D%2C%22html%22%3A%7B%7D%7D",scope:"",htmlWriteWay:"write",background:"undefined",visible:!0,onMount:e[2]||(e[2]=()=>{l.value=!1}),vueCode:t(A)},c({_:2},[v.value?{name:"vue",fn:r(()=>[n(t(v))]),key:"0"}:void 0]),1032,["vueCode"])]),_:1})])]),e[16]||(e[16]=g(`<div class="language-ts vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">ts</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">readonly</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">(params{</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  formData: Record</span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">&lt;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">string, any&gt;;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  schemaItem: ComponentConfig;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}) {</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">    return</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> params.formData.count </span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">===</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> &quot;1&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}</span></span></code></pre></div><h2 id="表单配置联动" tabindex="-1">表单配置联动 <a class="header-anchor" href="#表单配置联动" aria-label="Permalink to &quot;表单配置联动&quot;">​</a></h2>`,2)),a("ul",null,[a("li",null,[e[7]||(e[7]=s("开启即时配送第二项 ",-1)),e[8]||(e[8]=a("strong",null,"姓名",-1)),e[9]||(e[9]=s(" 修改为 ",-1)),e[10]||(e[10]=a("strong",null,"配送员",-1)),u(n(t(f),null,null,512),[[h,l.value]]),n(o,null,{default:r(()=>[n(t(k),{title:"",description:"",locale:"",select:"vue",order:"vue,react,html",github:"",gitlab:"",theme:"",lightTheme:"",darkTheme:"",stackblitz:"%7B%22show%22%3Afalse%7D",codesandbox:"%7B%22show%22%3Afalse%7D",codeplayer:"%7B%22show%22%3Afalse%7D",files:"%7B%22vue%22%3A%7B%7D%2C%22react%22%3A%7B%7D%2C%22html%22%3A%7B%7D%7D",scope:"",htmlWriteWay:"write",background:"undefined",visible:!0,onMount:e[3]||(e[3]=()=>{l.value=!1}),vueCode:t(D)},c({_:2},[y.value?{name:"vue",fn:r(()=>[n(t(y))]),key:"0"}:void 0]),1032,["vueCode"])]),_:1})])])])}}});export{F as __pageData,L as default};
---
+++ assets/guide_basics_form-node.md.CzYICg91.lean.js
@@ -0,0 +1,219 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/chunks/Visible.3CUeBz8_.js","assets/chunks/framework.Cd6hzTNw.js","assets/chunks/DefaultValue.CFVeO6wh.js"])))=>i.map(i=>d[i]);
+import{D as r,v as h,aX as d,p as g,C as b,c as v,o as E,aW as k,ag as m,G as a,ah as c,k as s,w as i,ax as u}from"./chunks/framework.Cd6hzTNw.js";import{L as f,N as y}from"./chunks/index.BpdcUPsw.js";const F=`<script setup lang="ts">
+import { ref } from "vue";
+import type { FormSchema } from "form-cook-render";
+
+const formData = ref({});
+const formSchema = ref<FormSchema>({
+  formAreaConfig: {
+    defaultCreateBtn: "提交",
+    defaultRestBtn: "重置",
+    attrs: {
+      labelWidth: "80px",
+    },
+  },
+  formContentConfigList: [
+    {
+      id: "1",
+      componentName: "Input",
+      componentType: "form",
+      formItemAttrs: { field: "name", label: "姓名" },
+      attrs: {
+        placeholder: "请输入姓名",
+      },
+      defaultValue: "齐天大圣",
+      visible: false,
+    },
+    {
+      id: "5",
+      componentName: "Switch",
+      componentType: "form",
+      formItemAttrs: { field: "delivery", label: "即时配送" },
+      attrs: {
+        disabled: true,
+      },
+      defaultValue: true,
+    },
+    {
+      id: "9",
+      componentName: "Input",
+      componentType: "form",
+      formItemAttrs: { field: "desc", label: "活动形式" },
+      attrs: {
+        type: "textarea",
+        placeholder: "请输入活动形式",
+        readonly: true,
+      },
+      defaultValue: "活动形式是只读的...",
+    },
+  ],
+});
+<\/script>
+
+<template>
+  <form-cook-render
+    v-model="formData"
+    v-model:form-schema="formSchema"
+    @submit="(data) => console.log('提交数据:', data)"
+    @reset="console.log('表单已重置')"
+  />
+</template>
+`,A=`<script setup lang="ts">
+import { ref } from "vue";
+import type { FormSchema } from "form-cook-render";
+
+const formData = ref({});
+const formSchema = ref<FormSchema>({
+  formAreaConfig: {
+    defaultCreateBtn: "提交",
+    defaultRestBtn: "重置",
+    attrs: {
+      labelWidth: "80px",
+    },
+  },
+  formContentConfigList: [
+    {
+      id: "1",
+      componentName: "Input",
+      componentType: "form",
+      formItemAttrs: { field: "name", label: "姓名" },
+      attrs: {
+        placeholder: "请输入姓名",
+      },
+      defaultValue: "齐天大圣",
+    },
+    {
+      id: "2",
+      componentName: "Select",
+      componentType: "form",
+      formItemAttrs: { field: "region", label: "所在地区" },
+      slots: {
+        default: {
+          componentName: "Option",
+          options: [
+            {
+              label: "上海",
+              value: "shanghai",
+            },
+            {
+              label: "北京",
+              value: "beijing",
+            },
+          ],
+        },
+      },
+      attrs: {
+        placeholder: "请选择地区",
+      },
+      defaultValue: "shanghai",
+    },
+    {
+      id: "3",
+      componentName: "SelectV2",
+      componentType: "form",
+      formItemAttrs: { field: "count", label: "活动次数" },
+      attrs: {
+        placeholder: "请选择次数",
+        options: [
+          {
+            label: "1次",
+            value: "1",
+          },
+          {
+            label: "2次",
+            value: "2",
+          },
+        ],
+      },
+      defaultValue: "2",
+    },
+    {
+      id: "5",
+      componentName: "Switch",
+      componentType: "form",
+      formItemAttrs: { field: "delivery", label: "即时配送" },
+      attrs: {},
+      defaultValue: true,
+    },
+    {
+      id: "6",
+      componentName: "Segmented",
+      componentType: "form",
+      formItemAttrs: { field: "location", label: "活动地点" },
+      attrs: {
+        options: ["家里", "公司", "学校"],
+      },
+      defaultValue: "学校",
+    },
+    {
+      id: "7",
+      componentName: "CheckboxGroup",
+      componentType: "form",
+      formItemAttrs: { field: "type", label: "活动类型" },
+      attrs: {},
+      slots: {
+        default: {
+          componentName: "Checkbox",
+          options: [
+            {
+              value: "促销活动",
+              name: "type",
+              label: "促销活动",
+            },
+            {
+              value: "线下活动",
+              name: "type",
+              label: "线下活动",
+            },
+          ],
+        },
+      },
+      defaultValue: ["促销活动"],
+    },
+    {
+      id: "8",
+      componentName: "RadioGroup",
+      componentType: "form",
+      formItemAttrs: { field: "resource", label: "资源类型" },
+      attrs: {},
+      slots: {
+        default: {
+          componentName: "Radio",
+          options: [
+            {
+              value: "赞助",
+              label: "赞助",
+            },
+            {
+              value: "场地",
+              label: "场地",
+            },
+          ],
+        },
+      },
+      defaultValue: "场地",
+    },
+    {
+      id: "9",
+      componentName: "Input",
+      componentType: "form",
+      formItemAttrs: { field: "desc", label: "活动形式" },
+      attrs: {
+        type: "textarea",
+        placeholder: "请输入活动形式",
+      },
+      defaultValue: "活动形式是...",
+    },
+  ],
+});
+<\/script>
+
+<template>
+  <form-cook-render
+    v-model="formData"
+    v-model:form-schema="formSchema"
+    @submit="(data) => console.log('提交数据:', data)"
+    @reset="console.log('表单已重置')"
+  />
+</template>
+`,Z=JSON.parse('{"title":"表单节点配置","description":"","frontmatter":{},"headers":[],"relativePath":"guide/basics/form-node.md","filePath":"guide/basics/form-node.md"}'),_={name:"guide/basics/form-node.md"},V=Object.assign(_,{setup(C){const l=r();h(async()=>{l.value=(await d(async()=>{const{default:t}=await import("./chunks/Visible.3CUeBz8_.js");return{default:t}},__vite__mapDeps([0,1]))).default});const e=g(!0),o=r();return h(async()=>{o.value=(await d(async()=>{const{default:t}=await import("./chunks/DefaultValue.CFVeO6wh.js");return{default:t}},__vite__mapDeps([2,1]))).default}),(t,n)=>{const p=b("ClientOnly");return E(),v("div",null,[n[2]||(n[2]=k("",3)),m(a(s(f),null,null,512),[[c,e.value]]),a(p,null,{default:i(()=>[a(s(y),{title:"",description:"",locale:"",select:"vue",order:"vue,react,html",github:"",gitlab:"",theme:"",lightTheme:"",darkTheme:"",stackblitz:"%7B%22show%22%3Afalse%7D",codesandbox:"%7B%22show%22%3Afalse%7D",codeplayer:"%7B%22show%22%3Afalse%7D",files:"%7B%22vue%22%3A%7B%7D%2C%22react%22%3A%7B%7D%2C%22html%22%3A%7B%7D%7D",scope:"",htmlWriteWay:"write",background:"undefined",visible:!0,onMount:n[0]||(n[0]=()=>{e.value=!1}),vueCode:s(A)},u({_:2},[o.value?{name:"vue",fn:i(()=>[a(s(o))]),key:"0"}:void 0]),1032,["vueCode"])]),_:1}),n[3]||(n[3]=k("",2)),m(a(s(f),null,null,512),[[c,e.value]]),a(p,null,{default:i(()=>[a(s(y),{title:"",description:"",locale:"",select:"vue",order:"vue,react,html",github:"",gitlab:"",theme:"",lightTheme:"",darkTheme:"",stackblitz:"%7B%22show%22%3Afalse%7D",codesandbox:"%7B%22show%22%3Afalse%7D",codeplayer:"%7B%22show%22%3Afalse%7D",files:"%7B%22vue%22%3A%7B%7D%2C%22react%22%3A%7B%7D%2C%22html%22%3A%7B%7D%7D",scope:"",htmlWriteWay:"write",background:"undefined",visible:!0,onMount:n[1]||(n[1]=()=>{e.value=!1}),vueCode:s(F)},u({_:2},[l.value?{name:"vue",fn:i(()=>[a(s(l))]),key:"0"}:void 0]),1032,["vueCode"])]),_:1})])}}});export{Z as __pageData,V as default};
